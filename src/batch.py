233f3d5f6ae9b92399a9bf74eadf79cb41804a5b
Jed-Nesbitt <jed1nesbitt@gmail.com>
2025-12-27 19:52:41 +1100

    An upgrade

--- src/batch.py
+++ src/batch.py
@@ -6,7 +6,7 @@ from typing import Any, Dict, Optional
 
 import pandas as pd
 
-from bond import DatedFixedRateBond, DayCount
+from src.bond import DatedFixedRateBond, DayCount
 
 
 def _to_date(x: Any) -> Optional[date]:
